2e69741465bde260cc4b47e98e57fbda7c8b1764
Erick-Brandao <erickbrandao1238barbosa@gmail.com>
2025-04-14 07:29:37 -0300

    frontlogin

---
+++ cad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <main>
+    <h1>PC HACKEADO COM SUCESSO</h1>
+    <div class="img">
+    <img src="jeff.jpeg" alt="">
+    <img src="jeff.jpeg" alt="">
+    </div>
+    </main>
+</body>
+</html>
